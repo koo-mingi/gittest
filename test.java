34c0a5515b0f3ec9acadd85615cb61522b80515e
mingikoo <ekzmfldks295@gmail.com>
2020-04-02 17:07:01 +0900

    Test.java merge

--- test.java
+++ test.java
@@ -3,10 +3,7 @@ pubic class Test{
 	System.out.println("Hello");
 	System.out.println("Hello World");
 	System.out.println("hotfix add");
-<<<<<<< HEAD
 	System.out.println("master add");
-=======
 	System.out.println("hotfix add2");
->>>>>>> hotfix
 	}
 }
